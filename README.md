609add86f09d7afe8ae95e5c5e6c9ebf631a85fe
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-13 19:05:05 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,25 +1,2 @@
 # terraform-aws-core-networking
 Terraform | Amazon Web Services (AWS) Core networking modules
-
-
-# Gatsby : Source REST API <img valign="bottom" align="right" height="35px" widht="35px" src="https://www.gatsbyjs.org/gatsby-monogram.svg" />
-
-
-### What is in there
-This GitHub repository is just a simple web application starter sourced from an external REST API.
-
-
-### What is Gatsby
-Gatsby is a blazing fast modern site generator for React. Gatsby is a free and open source framework based on React that helps developers build blazing fast websites and apps. Check out the website [gatsbyjs.org](https://gatsbyjs.org).
-
-
-### How to make it run
-```
-git clone https://github.com/mustapha-benmbarek/gatsby-source-rest-api
-cd gatsby-source-rest-api
-npm install 
-gatsby develop
-```
-
-### Support
-If you need support on that project or one of your gatsby project you are working on, feel free to reach out to me.
--- mod.main.tf
+++ mod.main.tf
@@ -1,53 +1,19 @@
 /*Networking Module | Virtual Private Cloud (VPC)*/
 module "vpcs" {
-  source    = "${path.module}/modules/vpc"
+  source    = "./modules/vpc"
   core-vpcs = local.lst-vpcs
 }
 
 /*Networking Module | VPC Subnet*/
 module "vpc-subnets" {
-  source           = "${path.module}/modules/vpc-subnet"
+  source           = "./modules/vpc-subnet"
   core-vpcs        = module.vpcs.ids
   core-vpc-subnets = local.lst-vpc-subnets
 }
 
-/*Networking Module | VPC DHCP Option Set*/
-module "vpc-dhcp-option-sets" {
-  source                    = "${path.module}/modules/vpc-dhcp-option-set"
-  core-vpcs                 = module.vpcs.ids
-  core-vpc-dhcp-option-sets = local.lst-vpc-dhcp-option-sets
-}
-
 /*Networking Module | VPC Internet Gateway*/
 module "vpc-internet-gateways" {
-  source                     = "${path.module}/modules/vpc-internet-gateway"
+  source                     = "./modules/vpc-internet-gateway"
   core-vpcs                  = module.vpcs.ids
   core-vpc-internet-gateways = local.lst-vpc-internet-gateways
-}
-
-/*Networking Module | VPC Egress-only Internet Gateway*/
-module "vpc-egress-only-internet-gateways" {
-  source                                 = "${path.module}/modules/vpc-egress-only-internet-gateway"
-  core-vpcs                              = module.vpcs.ids
-  core-vpc-egress-only-internet-gateways = local.lst-vpc-egress-only-internet-gateways
-}
-
-/*Networking Module | VPC Elastic IP*/
-module "vpc-elastic-ips" {
-  source               = "${path.module}/modules/vpc-elastic-ip"
-  core-vpc-elastic-ips = local.lst-vpc-elastic-ips
-}
-
-/*Networking Module | VPC NAT Gateway*/
-module "vpc-nat-gateways" {
-  source                = "${path.module}/modules/vpc-nat-gateway"
-  core-vpc-subnets      = module.vpc-subnets.ids
-  core-vpc-elastic-ips  = module.vpc-elastic-ips.ids
-  core-vpc-nat-gateways = local.lst-vpc-nat-gateways
-}
-
-/*Networking Module | VPC Managed Prefixy*/
-module "vpc-managed-prefix-lists" {
-  source                        = "${path.module}/modules/vpc-managed-prefix-list"
-  core-vpc-managed-prefix-lists = local.lst-vpc-managed-prefix-lists
 }
