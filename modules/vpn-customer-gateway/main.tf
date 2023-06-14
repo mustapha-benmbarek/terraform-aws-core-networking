0223e925ca7ff8a3003644e2590046785cd4ede9
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-14 13:11:21 +0400

    Initial commit

--- modules/vpn-customer-gateway/main.tf
+++ modules/vpn-customer-gateway/main.tf
@@ -2,7 +2,7 @@ resource "aws_customer_gateway" "this" {
   for_each   = var.core-vpn-customer-gateways
   type       = each.value.type
   bgp_asn    = each.value.asn
-  ip_address = each.value.ip-address-ipv4 != "" ? each.value.ip-address-ipv4 : null
+  ip_address = each.value.address-ipv4 != "" ? each.value.address-ipv4 : null
   tags = {
     Name = each.key
   }
